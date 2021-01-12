203cc2437ac2f98bb6f14eb3d03050ddcf4aea74
Justyna Koźlik <jkozlik96@gmail.com>
2021-01-12 16:41:49 +0100

    * Extract a table view (employee.fxml)

--- src/main/java/sample/controller/AppController.java
+++ src/main/java/sample/controller/AppController.java
@@ -1,64 +1,35 @@
 package sample.controller;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import sample.dto.EmployeeDto;
-import sample.rest.EmployeeRestClient;
-import sample.table.EmployeeTableModel;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
+import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
-import java.util.stream.Collectors;
 
 public class AppController implements Initializable {
 
-    private final EmployeeRestClient employeeRestClient;
-
-    public AppController(){
-        employeeRestClient = new EmployeeRestClient();
-    }
-
     @FXML
-    private TableView<EmployeeTableModel> tableView;
+    private Pane appPane;
+
+    public AppController() {
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-
-        TableColumn firstNameColumn = new TableColumn("First Name");
-        firstNameColumn.setMinWidth(100);
-        firstNameColumn.setCellValueFactory(new PropertyValueFactory<EmployeeTableModel, String>("firstName"));
-
-        TableColumn secondNameColumn = new TableColumn("Second Name");
-        secondNameColumn.setMinWidth(100);
-        secondNameColumn.setCellValueFactory(new PropertyValueFactory<EmployeeTableModel, String>("lastName"));
-
-        TableColumn salaryColumn = new TableColumn("Salary");
-        salaryColumn.setMinWidth(100);
-        salaryColumn.setCellValueFactory(new PropertyValueFactory<EmployeeTableModel, String>("salary"));
-
-        tableView.getColumns().addAll(firstNameColumn, secondNameColumn, salaryColumn);
-
-        ObservableList<EmployeeTableModel> data = FXCollections.observableArrayList();
-
-        loadEmployeeData(data);
-
-        tableView.setItems(data);
+        loadView();
     }
 
-    private void loadEmployeeData(ObservableList<EmployeeTableModel> data) {
+    private void loadView() {
 
-        Thread thread = new Thread(() -> {
-
-            List<EmployeeDto> employees = employeeRestClient.getEmployees();
-            data.addAll(employees.stream().map(EmployeeTableModel::of).collect(Collectors.toList()));
-        });
-        thread.start();
+        try {
+            BorderPane borderPane = FXMLLoader.load(getClass().getResource("/fxml/employee.fxml"));
+            appPane.getChildren().add(borderPane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/sample/controller/EmployeeController.java
@@ -0,0 +1,64 @@
+package sample.controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import sample.dto.EmployeeDto;
+import sample.rest.EmployeeRestClient;
+import sample.table.EmployeeTableModel;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+public class EmployeeController implements Initializable {
+
+    private final EmployeeRestClient employeeRestClient;
+
+    public EmployeeController(){
+        employeeRestClient = new EmployeeRestClient();
+    }
+
+    @FXML
+    private TableView<EmployeeTableModel> tableView;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        TableColumn firstNameColumn = new TableColumn("First Name");
+        firstNameColumn.setMinWidth(100);
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<EmployeeTableModel, String>("firstName"));
+
+        TableColumn secondNameColumn = new TableColumn("Second Name");
+        secondNameColumn.setMinWidth(100);
+        secondNameColumn.setCellValueFactory(new PropertyValueFactory<EmployeeTableModel, String>("lastName"));
+
+        TableColumn salaryColumn = new TableColumn("Salary");
+        salaryColumn.setMinWidth(100);
+        salaryColumn.setCellValueFactory(new PropertyValueFactory<EmployeeTableModel, String>("salary"));
+
+        tableView.getColumns().addAll(firstNameColumn, secondNameColumn, salaryColumn);
+
+        ObservableList<EmployeeTableModel> data = FXCollections.observableArrayList();
+
+        loadEmployeeData(data);
+
+        tableView.setItems(data);
+    }
+
+    private void loadEmployeeData(ObservableList<EmployeeTableModel> data) {
+
+        Thread thread = new Thread(() -> {
+
+            List<EmployeeDto> employees = employeeRestClient.getEmployees();
+            data.addAll(employees.stream().map(EmployeeTableModel::of).collect(Collectors.toList()));
+        });
+        thread.start();
+    }
+}
