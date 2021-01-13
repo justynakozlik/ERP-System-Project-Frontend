7f560c488901759d777566aa2a79b73fc352987c
Justyna Koźlik <jkozlik96@gmail.com>
2021-01-13 14:58:46 +0100

    + Create "Add employee" controller

--- src/main/java/sample/controller/EmployeeController.java
+++ src/main/java/sample/controller/EmployeeController.java
@@ -3,14 +3,22 @@ package sample.controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import sample.dto.EmployeeDto;
 import sample.rest.EmployeeRestClient;
 import sample.table.EmployeeTableModel;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -19,16 +27,46 @@ import java.util.stream.Collectors;
 public class EmployeeController implements Initializable {
 
     private final EmployeeRestClient employeeRestClient;
+    @FXML
+    private Button addButton;
+    @FXML
+    private Button viewButton;
+    @FXML
+    private Button editButton;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private TableView<EmployeeTableModel> tableView;
+
 
     public EmployeeController(){
         employeeRestClient = new EmployeeRestClient();
     }
 
-    @FXML
-    private TableView<EmployeeTableModel> tableView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        initializeAddEmployeeButton();
+        initializeTableView();
+    }
+
+    private void initializeAddEmployeeButton() {
+        addButton.setOnAction((x) -> {
+            Stage addEmployeeStage = new Stage();
+            addEmployeeStage.initStyle(StageStyle.UNDECORATED);
+            addEmployeeStage.initModality(Modality.APPLICATION_MODAL);
+            try {
+                Parent addEmployeeParent = FXMLLoader.load(getClass().getResource("/fxml/add-employee.fxml"));
+                Scene scene = new Scene(addEmployeeParent, 500, 400);
+                addEmployeeStage.setScene(scene);
+                addEmployeeStage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void initializeTableView() {
         tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 
         TableColumn firstNameColumn = new TableColumn("First Name");
