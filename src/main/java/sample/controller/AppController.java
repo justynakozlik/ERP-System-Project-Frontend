ccc066a5e576f0525049b38453fe2de71cdf5264
Justyna Koźlik <jkozlik96@gmail.com>
2021-01-15 16:09:03 +0100

    * Implementation of the behavior of menu buttons

--- src/main/java/sample/controller/AppController.java
+++ src/main/java/sample/controller/AppController.java
@@ -3,8 +3,13 @@ package sample.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
@@ -12,24 +17,71 @@ import java.util.ResourceBundle;
 
 public class AppController implements Initializable {
 
+    private final static String EMPLOYEE_MODULE_VIEW = "/fxml/employee.fxml";
+    private final static String WAREHOUSE_MODULE_VIEW = "/fxml/warehouse.fxml";
+    private final static String LOGIN_VIEW = "/fxml/login.fxml";
+
+    @FXML
+    private BorderPane appBorderPane;
+
+    @FXML
+    private MenuItem employeeModuleMenuItem;
+
+    @FXML
+    private MenuItem warehouseModuleMenuItem;
+
+    @FXML
+    private MenuItem logoutMenuItem;
+
+    @FXML
+    private MenuItem exitMenuItem;
+
     @FXML
     private Pane appPane;
 
-    public AppController() {
-    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        loadView();
+        loadDefaultView();
+        initializeMenuItems();
     }
 
-    private void loadView() {
+    private void initializeMenuItems() {
+        warehouseModuleMenuItem.setOnAction(x -> loadModuleView(WAREHOUSE_MODULE_VIEW));
+        employeeModuleMenuItem.setOnAction(x -> loadModuleView(EMPLOYEE_MODULE_VIEW));
+        logoutMenuItem.setOnAction(x -> logout());
+        exitMenuItem.setOnAction(x -> getStage().close());
 
+    }
+
+    private void logout() {
         try {
-            BorderPane borderPane = FXMLLoader.load(getClass().getResource("/fxml/employee.fxml"));
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource(LOGIN_VIEW));
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(new Scene(root, 600, 400));
+            stage.show();
+            getStage().close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't load fxml file: " + LOGIN_VIEW);
+        }
+    }
+
+    private void loadDefaultView() {
+        loadModuleView(EMPLOYEE_MODULE_VIEW);
+    }
+
+    private void loadModuleView(String viewPath) {
+        appPane.getChildren().clear();
+        try {
+            BorderPane borderPane = FXMLLoader.load(getClass().getResource(viewPath));
             appPane.getChildren().add(borderPane);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Can't load fxml file: " + viewPath);
         }
+    }
+
+    private Stage getStage() {
+        return (Stage) appBorderPane.getScene().getWindow();
     }
 }
